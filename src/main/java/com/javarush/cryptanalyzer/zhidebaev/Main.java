d11cd2c483d3237d8a4a67db9aab5a0be859dfb2
AlibekZhidebaev <foxmal81@gmail.com>
2023-03-28 09:04:49 +0600

    realization

--- src/main/java/com/javarush/cryptanalyzer/zhidebaev/Main.java
+++ src/main/java/com/javarush/cryptanalyzer/zhidebaev/Main.java
@@ -2,9 +2,12 @@ package com.javarush.cryptanalyzer.zhidebaev;
 
 import com.javarush.cryptanalyzer.zhidebaev.view.GUI;
 
+import static com.javarush.cryptanalyzer.zhidebaev.constants.CryptoAlphabet.ALPHABET;
+
 public class Main {
     public static void main(String[] args) {
 //1234567890
         GUI gui = new GUI();
+        System.out.println(ALPHABET.length());
     }
 }
--- src/main/java/com/javarush/cryptanalyzer/zhidebaev/view/GUI.java
+++ src/main/java/com/javarush/cryptanalyzer/zhidebaev/view/GUI.java
@@ -4,38 +4,45 @@ import javax.swing.*;
 
 import static java.awt.TextArea.SCROLLBARS_VERTICAL_ONLY;
 
-public class GUI extends JFrame {
-    private JTextField txtKey;
-    private JTextArea txtIn;
-    private JTextArea txtOut;
-    private JScrollPane scrollPane;
-    public GUI(){
-        setSize(600,400);
+public class GUI  extends JFrame{
+   private JPanel panel;
+   private JTextField txtKey;
+   private JTextArea txtIn;
+   private JTextArea txtOut;
+  private JScrollPane scrollPane;
+public GUI(){
+    setSize(600,400);
     setTitle("Dr. Payne's Secret Message App");
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     getContentPane().setLayout(null);
 
     txtIn = new JTextArea(); //          JTextArea
-    txtIn.setBounds(10, 11, 564, 140);
-         scrollPane = new JScrollPane(txtIn);
-       // scrollPane.setViewportView(txtIn);
-       // scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-   getContentPane().add(txtIn);
-      getContentPane().add(scrollPane);
+    txtIn.setRows(10);
+    txtIn.setColumns(110);
+
+    // txtIn.setBounds(10, 11, 564, 140);
+
+      getContentPane().add(txtIn);
+    scrollPane = new JScrollPane(txtIn, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER );
+    scrollPane.setVisible(true);
+    getContentPane().add(scrollPane);
+    //getContentPane().add(txtIn);
+
     txtOut = new JTextArea(); //          JTextArea
     txtOut.setBounds(10, 210, 564, 140);
     getContentPane().add(txtOut);
-        txtKey = new JTextField();
-        txtKey.setBounds(258, 173, 44, 20);
-        getContentPane().add(txtKey);
-        JLabel lblKey = new JLabel("Key:");
-        lblKey.setBounds(202, 176, 46, 14);
-        getContentPane().add(lblKey);
-        JButton btnEncodedecode = new JButton("Encode/Decode");
-        btnEncodedecode.setBounds(312, 172, 144, 23);
-        getContentPane().add(btnEncodedecode);
+    txtKey = new JTextField();
+    txtKey.setBounds(258, 173, 44, 20);
+    getContentPane().add(txtKey);
+    JLabel lblKey = new JLabel("Key:");
+    lblKey.setBounds(202, 176, 46, 14);
+    getContentPane().add(lblKey);
+    JButton btnEncodedecode = new JButton("Encode/Decode");
+    btnEncodedecode.setBounds(312, 172, 144, 23);
+    getContentPane().add(btnEncodedecode);
 
-        setVisible(true);
+    setVisible(true);
 
-    }
+}
 }
