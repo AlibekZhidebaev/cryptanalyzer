6aacb59c301ede1f74659f70949ad18d6a5e98e1
MyPC <foxmal81@gmail.com>
2023-04-09 16:49:32 +0600

    program mode logic

--- src/main/java/com/javarush/cryptanalyzer/zhidebaev/services/Decoder.java
+++ src/main/java/com/javarush/cryptanalyzer/zhidebaev/services/Decoder.java
@@ -3,13 +3,42 @@ package com.javarush.cryptanalyzer.zhidebaev.services;
 import com.javarush.cryptanalyzer.zhidebaev.entity.Result;
 import com.javarush.cryptanalyzer.zhidebaev.exception.ApplicationException;
 import com.javarush.cryptanalyzer.zhidebaev.repository.ResultCode;
+import com.javarush.cryptanalyzer.zhidebaev.constants.CryptoAlphabet;
+
+import java.io.FileReader;
+import java.io.FileWriter;
 
 public class Decoder implements Function{
+
+    String sourceFile = "text//encoded.txt";
+    String destinationFile = "text//output.txt";
+    int key =3;
+
+    public char encode(int key, char symbol) {
+        int offsetNumber = -key;
+        char changedCharacter = symbol;
+        if (CryptoAlphabet.ALPHABET.lastIndexOf(symbol) != -1) {
+            int positionIndex = CryptoAlphabet.ALPHABET.lastIndexOf(symbol) + offsetNumber;
+            if (positionIndex > (CryptoAlphabet.ALPHABET_SIZE - 1)) {
+                positionIndex = positionIndex - CryptoAlphabet.ALPHABET_SIZE;
+            }
+            if (positionIndex < 0) {
+                positionIndex = positionIndex + CryptoAlphabet.ALPHABET_SIZE;
+            }
+            changedCharacter = CryptoAlphabet.ALPHABET.charAt(positionIndex);
+        }
+        return changedCharacter;
+    }
+
     @Override
     public Result execute() {
-        try {
+        try (FileReader reader = new FileReader(sourceFile); FileWriter writer = new FileWriter(destinationFile)) {
             System.out.println("works Decode");
-        }catch(Exception ex) {
+            while (reader.ready()) {
+                char real = (char) reader.read();
+                writer.write(encode(key, real));
+            }
+        } catch (Exception ex) {
             return new Result(ResultCode.ERROR, new ApplicationException("Decode error", ex));
         }
         return new Result(ResultCode.OK);
--- src/main/java/com/javarush/cryptanalyzer/zhidebaev/services/Encoder.java
+++ src/main/java/com/javarush/cryptanalyzer/zhidebaev/services/Encoder.java
@@ -1,15 +1,45 @@
 package com.javarush.cryptanalyzer.zhidebaev.services;
 
+import com.javarush.cryptanalyzer.zhidebaev.constants.CryptoAlphabet;
 import com.javarush.cryptanalyzer.zhidebaev.entity.Result;
 import com.javarush.cryptanalyzer.zhidebaev.exception.ApplicationException;
 import com.javarush.cryptanalyzer.zhidebaev.repository.ResultCode;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+
 public class Encoder implements Function{
+    String sourceFile = "text//input.txt";
+    String destinationFile = "text//encoded.txt";
+    int key =3;
+
+    public char encode(int key, char symbol) {
+        int offsetNumber = key;
+        char changedCharacter = symbol;
+        
+        if (CryptoAlphabet.ALPHABET.lastIndexOf(symbol) != -1) {
+            int positionIndex = CryptoAlphabet.ALPHABET.lastIndexOf(symbol) + offsetNumber;
+            if (positionIndex > (CryptoAlphabet.ALPHABET_SIZE - 1)) {
+                positionIndex = positionIndex - CryptoAlphabet.ALPHABET_SIZE;
+            }
+            if (positionIndex < 0) {
+                positionIndex = positionIndex + CryptoAlphabet.ALPHABET_SIZE;
+            }
+            changedCharacter = CryptoAlphabet.ALPHABET.charAt(positionIndex);
+        }
+        return changedCharacter;
+    }
+
     @Override
     public Result execute() {
-        try {
+        try (FileReader reader = new FileReader(sourceFile); FileWriter writer = new FileWriter(destinationFile)) {
             System.out.println("works Encode");
-        }catch(Exception ex) {
+            while (reader.ready()) {
+                char real = (char) reader.read();
+                writer.write(encode(key, real));
+            }
+
+        } catch (Exception ex) {
             return new Result(ResultCode.ERROR, new ApplicationException("Encode error", ex));
         }
         return new Result(ResultCode.OK);
