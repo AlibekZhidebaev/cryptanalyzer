e54a07604d07fb344f8fdbf94a9dfe97024a594b
AlibekZhidebaev <foxmal81@gmail.com>
2023-04-17 16:48:30 +0600

    implementation_statanalysis

--- src/main/java/com/javarush/cryptanalyzer/zhidebaev/services/StatAnalysis.java
+++ src/main/java/com/javarush/cryptanalyzer/zhidebaev/services/StatAnalysis.java
@@ -3,12 +3,52 @@ package com.javarush.cryptanalyzer.zhidebaev.services;
 import com.javarush.cryptanalyzer.zhidebaev.entity.Result;
 import com.javarush.cryptanalyzer.zhidebaev.exception.ApplicationException;
 import com.javarush.cryptanalyzer.zhidebaev.repository.ResultCode;
+import com.javarush.cryptanalyzer.zhidebaev.utilities.ReadingFromFile;
+import com.javarush.cryptanalyzer.zhidebaev.utilities.TotalStatisticsOfSymbols;
+import com.javarush.cryptanalyzer.zhidebaev.utilities.WritingToFile;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatAnalysis implements Function {
+    private String inputFile = "text\\encoded.txt";
+    private String dictionaryFile = "text\\dictionary.txt";
+    private String outputFile = "text\\output.txt";
+
+    List<Character> sourceTextSymbolFrequency = new ArrayList<>();
+    List<Character> symbolFrequencyDictionary = new ArrayList<>();
+    Map<Character,Character > symbolMappings = new HashMap<>();
+    List<Character> outputTextSymbols = new ArrayList<>();
+    StringBuilder outputText = new StringBuilder();
+
+
     @Override
     public Result execute(String[] commandParameters) {
         try {
             System.out.println("works StatAnalysis");
+            // --------------------------------------------------------
+            char[] sourceTextSymbols = new ReadingFromFile(inputFile).getFileAsArrayOfCharacters();
+            char[] dictionarySymbols = new ReadingFromFile(dictionaryFile).getFileAsArrayOfCharacters();
+            sourceTextSymbolFrequency = new TotalStatisticsOfSymbols(sourceTextSymbols).getSymbols();
+            symbolFrequencyDictionary = new TotalStatisticsOfSymbols(dictionarySymbols).getSymbols();
+
+            int mapIndex = Math.min(sourceTextSymbolFrequency.size(), symbolFrequencyDictionary.size());
+            for(int i=0; i<mapIndex; i++) {
+                symbolMappings.put(sourceTextSymbolFrequency.get(i), symbolFrequencyDictionary.get(i));
+            }
+
+            for(int i=0; i<sourceTextSymbols.length; i++) {
+                if(symbolMappings.containsKey(sourceTextSymbols[i]))
+                    outputText.append(sourceTextSymbols[i] = symbolMappings.get(sourceTextSymbols[i]));
+                else outputText.append(sourceTextSymbols[i] = sourceTextSymbols[i]);
+            }
+
+            new WritingToFile(outputFile,outputText.toString());
+            System.out.println(symbolMappings);
+
+            // ---------------------------------------------------------
         }catch(Exception ex) {
             return new Result(ResultCode.ERROR, new ApplicationException("StatAnalysis", ex));
         }
---
+++ src/main/java/com/javarush/cryptanalyzer/zhidebaev/utilities/TotalStatisticsOfSymbols.java
@@ -0,0 +1,56 @@
+package com.javarush.cryptanalyzer.zhidebaev.utilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TotalStatisticsOfSymbols {
+    char[] textChars;
+    Map<Character, Integer> symbolFrequencyMap = new HashMap<Character, Integer>(); // -- Map --
+    List<Character> symbols = new ArrayList<>(); // -- список ключей Map --
+    List<Integer> frequency = new ArrayList<>();   // -- список значений ключей Map --
+    int totalNumber = 1;
+
+    // -- Метод получения списка значений ключей Map --
+
+    public List<Character> getSymbols() {
+        return symbols;
+    }
+
+
+    public TotalStatisticsOfSymbols(char[] textChars) {
+        this.textChars = textChars;
+
+        // -- Создание Map из массива символов (ключ = символ, значение = частотность) --
+
+        for (int i = 0; i < textChars.length; i++) {
+            if(!Character.isWhitespace(textChars[i]) || textChars[i] == ' ')
+                if (symbolFrequencyMap.containsKey(textChars[i]))
+                    symbolFrequencyMap.put(textChars[i], symbolFrequencyMap.get(textChars[i]) + 1);
+                else
+                    symbolFrequencyMap.put(textChars[i], totalNumber);
+        }
+
+        // -- Сортировка Map по значениям ключей с добавлением ключей и их значений в List --
+
+        for (HashMap.Entry<Character,Integer> e : symbolFrequencyMap.entrySet()) {
+            Integer value = e.getValue();
+            boolean isAdded = false;
+            for (int i = 0; i < frequency.size(); i++) {
+                if (value > frequency.get(i)) { // -- условие сортировки значений
+                    frequency.add(i, value);
+                    symbols.add(i, e.getKey());
+                    isAdded = true;
+                    break;
+                }
+            }
+            if (!isAdded) {
+                frequency.add(value);
+                symbols.add(e.getKey());
+            }
+        }
+    }
+
+  //  public List<Integer> getFrequency() { return frequency; }
+}
